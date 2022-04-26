02ae8e2e0966eb39813c6293fa4c84c15453d081
CamiloEspindola <cespindola@unal.edu.co>
2022-04-26 01:10:16 -0500

    aa

--- zooAnimales/anfibio.py
+++ zooAnimales/anfibio.py
@@ -1,42 +1,42 @@
 from zooAnimales.animal import Animal
 
 class Anfibio(Animal):
-    totalAnfibios=0
-    listado =[]
-    ranas=0
-    salamandras=0
-    def __init__(self,nombre,edad,habitat,genero,colorPiel,venenoso):
-        super().__init__(nombre, edad, habitat, genero)
-        self._colorPiel = colorPiel
-        self._venenoso = venenoso
-        Anfibio.totalAnfibios+=1
-        self.listado.append(self)
-        
-    @classmethod
-    def cantidadAnfibios(cls): 
-        return cls.totalAnfibios
-    
-    @classmethod
-    def crearRana(cls, nombre, edad, genero):
-        cls.ranas+=1
-        return Anfibio(nombre, edad, "selva", genero, "rojo", True)
-        
-    @classmethod
-    def crearSalamandra(cls,nombre, edad, genero):
-        cls.salamandras+=1
-        return Anfibio(nombre, edad, "selva", genero, "negro y amarillo", False)
+    _listado = []
+    ranas = 0
+    salamandras = 0
 
-    def movimiento(self):
-        return "saltar"
-        
+    def __init__(self, nombre, edad, habitat, genero, color, venenoso):
+        super().__init__(nombre, edad, habitat, genero)
+        self._colorPiel = color
+        self._venenoso = venenoso
+        Anfibio._listado.append(self)
+
+    def getColorPiel(self):
+        return self._colorPiel
+
     def isVenenoso(self):
         return self._venenoso
-    
-    def setColorPiel(self,a):
-        self._colorPiel=a
-    def getColorPiel(self): 
-        return self._colorPiel
-    def setVenenoso(self,a):
-        self._venenoso=a
-    def getVenenoso(self): 
-        return self._venenoso
+
+    def setColorPiel(self, color):
+        self._colorPiel = color
+
+    def setVenenoso(self, venenoso):
+        self._venenoso = venenoso
+
+    @staticmethod
+    def movimiento():
+        return "saltar"
+
+    @classmethod
+    def cantidadAnfibios(cls):
+        return len(cls._listado)
+
+    @classmethod
+    def crearRana(cls, nombre, edad, genero):
+        cls.ranas += 1
+        return Anfibio(nombre, edad, "selva", genero, "rojo", True)
+
+    @classmethod
+    def crearSalamandra(cls, nombre, edad, genero):
+        cls.salamandras += 1
+        return Anfibio(nombre, edad, "selva", genero, "negro y amarillo", False)
